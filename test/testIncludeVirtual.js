180517d54afef64ac60f6e164e200a38bdf1383a
Glenn Nelson <glenn@hexcoder.us>
2013-10-02 17:45:48 -0400

    Issue #4 - Fix virtual file includes to be relative where appropriate

--- test/testIncludeVirtual.js
+++ test/testIncludeVirtual.js
@@ -7,12 +7,12 @@ var ssi = require("../index.js");
 describe("#include virtual", function() {
 	var parser = new ssi("test/html", "", "");
 
-	it("virtual-same should contain \"ROOT\"", function() {
+	it("virtual-same should contain \"LEVEL2\"", function() {
 		var filename = "test/html/level1/level2/virtual-same.shtml";
 		var contents = fs.readFileSync(filename, {encoding: "utf8"});
 		var results = parser.parse(filename, contents);
 	
-		assert.equal("ROOT\n\n", results.contents);
+		assert.equal("LEVEL2\n\n", results.contents);
 	});
 
 	it("virtual-relative should contain \"LEVEL1\"", function() {
@@ -21,6 +21,14 @@ describe("#include virtual", function() {
 		var results = parser.parse(filename, contents);
 
 		assert.equal("LEVEL1\n\n", results.contents);
+	});
+
+	it("virtual-absolute should contain \"ROOT\"", function() {
+		var filename = "test/html/level1/level2/virtual-absolute.shtml";
+		var contents = fs.readFileSync(filename, {encoding: "utf8"});
+		var results = parser.parse(filename, contents);
+
+		assert.equal("ROOT\n\n", results.contents);
 	});
 });
 
